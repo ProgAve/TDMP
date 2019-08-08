273e8ef09be7e38c5055b2f902f63ca993938d81
AvE <118mk68@gmail.com>
2019-08-08 15:44:41 +0300

    Python test update

--- Python/Test/test.py
+++ Python/Test/test.py
@@ -1,1 +1,12 @@
-print("Hello")
+class Person:
+    def __init__(self,name="",age=30):
+        self.__name = name
+        self.__age = age
+    def name(self):
+        return self.__name
+    def age(self):
+        return self.__age
+
+man = Person("Oxana")
+man.__setattr__("__age",48)
+print("Hello, ",man.name()," ", man.age())
